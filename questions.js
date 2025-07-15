99af371a95736f4c9813cb501f87b45998d335c5
TND4-ZQJS <tnd4office@gmail.com>
2025-07-15 17:23:25 +0800

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,57 @@
+const questions = [
+  {
+    id: 1,
+    question: "When buying life insurance, an individual will benefit from ___________?",
+    options: {
+      A: "compelled savings",
+      B: "good health",
+      C: "investment return",
+      D: "long life"
+    },
+    answer: "A"
+  },
+  {
+    id: 2,
+    question: "The first stage in Risk Management is",
+    options: {
+      A: "transfer",
+      B: "control",
+      C: "identification",
+      D: "analysis"
+    },
+    answer: "C"
+  },
+  {
+    id: 3,
+    question: "The following differentiates insurance from gambling:",
+    options: {
+      A: "Insurable risks whose financial value is measurable",
+      B: "Inevitable losses beyond the control of the insured",
+      C: "Homogeneous or similar risks in large numbers",
+      D: "Insurable interest in the subject matter insured"
+    },
+    answer: "D"
+  },
+  {
+    id: 4,
+    question: "An example of moral hazard is",
+    options: {
+      A: "dishonest employees",
+      B: "engaging in dangerous sports",
+      C: "unguarded premises",
+      D: "storage of hazardous chemicals"
+    },
+    answer: "A"
+  },
+  {
+    id: 5,
+    question: "The government and society as a whole are solely responsible for this type of risks:",
+    options: {
+      A: "Particular risks",
+      B: "Speculative risks",
+      C: "Pure risks",
+      D: "Fundamental risks"
+    },
+    answer: "D"
+  }
+];
